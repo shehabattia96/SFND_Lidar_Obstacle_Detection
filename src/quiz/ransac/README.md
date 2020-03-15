03b6acfe3907999c47345f03aa0731c487d8ac71
Shehab Attia <shehabattia96@gmail.com>
2020-03-15 02:10:47 -0400

    Implemented plane RANSAC fitering from scratch. Compared the performance of the custom algorithm with PointCloud Library's RANSAC implementation

--- src/quiz/ransac/README.md
+++ src/quiz/ransac/README.md
@@ -2,7 +2,7 @@
 
 ## implementation plan
 
-### hyper parameters
+### hyper-parameters
 
 - distanceTolerance -> The distance at which a point will be considered an inlier
 - maxIterations -> The number of random samples we'll pull and fit
@@ -10,12 +10,13 @@
 ### assumptions
 
 - rand() returns a number > cloud.size()
+- rand() returns pseudo-random integers for the random sampling to occur
 
 ### calculating distance of a point from a line
 
-#### within two parallel lines approach
+#### Approach 1: within two parallel lines approach
 
-- Find the min and max points in the dataset
+- Given two starting points (x1, y1) and (x2, y2).
 - Two points define a line `y = mx + c`, where
   - `m = (y1 - y2) / (x1 - x2)`
   - `c = y1 - m * x1`
@@ -41,10 +42,37 @@ float Lc = ((2 * std::sqrt((distanceTol*distanceTol) * (m*m) + (distanceTol*dist
 - `d^2 = x1^2 -2*x1*(x1 - (y1 - Lc) / m)/2 + ((x1 - (y1 - Lc) / m)/2)^2 + y1^2 - 2*y1*(((m * x1 + Lc) - y1)/2) + (((m * x1 + Lc) - y1)/2)^2`
   -Worlfram Alpha friendly equation: `T = ((-2 sqrt(d^2 m^2 + d^2 - m^2 x1^2 + 2 m x1 y1 - y1^2))/m + y1/m^2 - m x1 + x1/m + 3 y1)/(1/m^2 + 1) where m=1 and x1=-1 and y1=0 and d=1`
 
-#### distance from center approach (may be costly to perform perpendicular distance calculation for each point on a large dataset)
+#### Approach 2: distance from center approach (may be costly to perform perpendicular distance calculation for each point on a large dataset)
 
+- Given two starting points (x1, y1) and (x2, y2).
 - Two points define a line `Ax + By + C = 0`, where
   - `A = y1 - y2`
-  - `B = x1- x2`
+  - `B = x1 - x2`
   - `C = x1 * y2 - x2 * y1`
 - The perpendicular distance of a point P from the line is `abs(A * Px + B * Py + C) / sqrt(A * A + B * B)`
+
+##### Conclusion from comparing approach 1 and approach 2
+
+- On a 2D dataset, approach 1 performed the line finding in (154, 178, 194 microseconds) and apprach 2 performed the line finding in (221, 220 and 184 microseconds). This is a small sample size and if I ran the test iteratively for longer, I may find that they perform very similarly because of the result from the 3D dataset.
+
+- On a 3D dataset, approach 1 performed the line finding in (2765, 2713 microseconds) and apprach 2 performed the line finding in (2716, 2713 microseconds).
+
+- Lastly, I would have had to hardcode two points verify each method's accuracy. I did not do this. But visually, they look like they're finding the optimal line well.
+
+### calculating the distance of a point from a plane
+
+- Given three starting points (x1, y1, z1), (x2, y2, z2) and (x3, y3, z3)
+- Calculate two vectors `v1 = (x2 - x1, y2 - y1, z2 - z1)` and `v2 = (x3 - x1, y3 - y1, z3 - z1)`
+- Calculate the cross product of V = v1 x v2. `Vx = (y2 − y1) * (z3 − z1) − (z2 − z1) * (y3 − y1)`. `Vy = (z2 - z1) * (x3 - x1) - (x2 - x1) * (z3 - z1)`. `Vz = (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1)`
+- Write the plane equation in standard form: Ax + By + Cz + D = 0 with reference to Point 1 (x1, y1, z1):
+  - A, B, C = Vx, Vy and Vz respectively.
+  - `D = -1 * (A * x1 + B * y1 + C * z1)`
+- To calculate the distance between a point and a plane, use the D found in standard form:
+  - `Distance =  fabs( A * x + B * y + C * z + D) / sqrt(A * A + B * B + C * C)`
+
+#### Comparing the performance of the custom RANSAC algorithm with PCL's algorithm
+
+- At first, the custom algorithm appeared to be twice slower than the PCL algorithm using hyper-parameters (MaxIter=10, disTol=0.5): On a 3D dataset, the custom algorith separated the plane in (15095, 14815 and 11406 microseconds). The PCL algorithm separate the plane in (9118, 7338 and 7424 microseconds).
+  - However, after playing with the hyper-parameters, I noticed that the PCL algorithm stops iterating when it's found an optimal solution BEFORE hitting max iterations, hence cutting the computation time.
+    - If I lower the hyper-parameters to (MaxIter=5, disTol=0.5), the custom algorithm also separates the plane in (7401, 6861 and 7394 micro-seconds)!
+      - Optimizing hyper-parameters is key, but also finding the "good enough" solution may be a good stopping point for an iterative algorithm like the RANSAC.
--- src/quiz/ransac/ransac2d.cpp
+++ src/quiz/ransac/ransac2d.cpp
@@ -61,7 +61,7 @@ pcl::visualization::PCLVisualizer::Ptr initScene()
   	return viewer;
 }
 
-std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol)
+std::unordered_set<int> Ransac2DLineUsingBounds(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol)
 {
 	auto startTime = std::chrono::steady_clock::now();
 	std::unordered_set<int> inliersResult;
@@ -110,11 +110,149 @@ std::unordered_set<int> Ransac(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int ma
 	
 	
 	auto endTime = std::chrono::steady_clock::now();
-	auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
-	std::cout << "Line RANSAC elapsedTime: " << elapsedTime.count() << " ms." << std::endl;
+	auto elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+	std::cout << "Line RANSAC elapsedTime: " << elapsedTime.count() << " micro-seconds." << std::endl;
 	
 	return inliersResult;
 
+}
+
+std::unordered_set<int> Ransac2DLineUsingNormalDistance(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol)
+{
+	auto startTime = std::chrono::steady_clock::now();
+	std::unordered_set<int> inliersResult;
+	srand(time(NULL)); // initialize random number generator
+
+	for (int iteration = 0; iteration < maxIterations; iteration += 1) {
+
+		std::unordered_set<int> inliers;
+		while (inliers.size() < 2) {
+			inliers.insert(rand()%(cloud->points.size()));
+		}
+
+		float x1, x2, y1, y2;
+		auto inliersIterator = inliers.begin();
+		x1 = cloud->points[*inliersIterator].x;
+		y1 = cloud->points[*inliersIterator].y;
+		inliersIterator++;
+		x2 = cloud->points[*inliersIterator].x;
+		y2 = cloud->points[*inliersIterator].y;
+
+		float a = (y1 - y2);
+		float b = (x2 - x1);
+		float c = (x1 * y2 - x2 * y1);
+
+		for (int index = 0; index < cloud->points.size(); index += 1) {
+			if (inliers.count(index) > 0) continue;
+			pcl::PointXYZ point = cloud->points[index];
+			float pointX = point.x;
+			float pointY = point.y;
+			float distance = fabs(a * pointX + b * pointY + c) / sqrt(a * a + b * b);
+			if (distance <= distanceTol)
+				inliers.insert(index);
+		}
+
+		if (inliers.size() > inliersResult.size()) {
+			inliersResult = inliers;
+		}
+	}
+	
+	
+	auto endTime = std::chrono::steady_clock::now();
+	auto elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+	std::cout << "Line RANSAC elapsedTime: " << elapsedTime.count() << " micro-seconds." << std::endl;
+	
+	return inliersResult;
+}
+
+std::unordered_set<int> Ransac3DLineUsingNormalDistance(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol)
+{
+	auto startTime = std::chrono::steady_clock::now();
+	std::unordered_set<int> inliersResult;
+	srand(time(NULL)); // initialize random number generator
+
+	for (int iteration = 0; iteration < maxIterations; iteration += 1) {
+
+		std::unordered_set<int> inliers;
+		while (inliers.size() < 3) {
+			inliers.insert(rand()%(cloud->points.size()));
+		}
+
+		float x1, x2, x3, y1, y2, y3, z1, z2, z3;
+		auto inliersIterator = inliers.begin();
+		x1 = cloud->points[*inliersIterator].x;
+		y1 = cloud->points[*inliersIterator].y;
+		z1 = cloud->points[*inliersIterator].z;
+		inliersIterator++;
+		x2 = cloud->points[*inliersIterator].x;
+		y2 = cloud->points[*inliersIterator].y;
+		z2 = cloud->points[*inliersIterator].z;
+		inliersIterator++;
+		x3 = cloud->points[*inliersIterator].x;
+		y3 = cloud->points[*inliersIterator].y;
+		z3 = cloud->points[*inliersIterator].z;
+
+		// explanation for calculating these coefficients is in the README.md
+		float A = (y2 - y1) * (z3 - z1) - (z2 - z1) * (y3 - y1);
+		float B = (z2 - z1) * (x3 - x1) - (x2 - x1) * (z3 - z1);
+		float C = (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1);
+		float D = -1 * (A * x1 + B * y1 + C * z1);
+
+		for (int index = 0; index < cloud->points.size(); index += 1) {
+			if (inliers.count(index) > 0) continue;
+			pcl::PointXYZ point = cloud->points[index];
+			float pointX = point.x;
+			float pointY = point.y;
+			float pointZ = point.z;
+
+			float distance =  fabs( A * pointX + B * pointY + C * pointZ + D) / sqrt(A * A + B * B + C * C);
+			
+			if (distance <= distanceTol)
+				inliers.insert(index);
+		}
+
+		if (inliers.size() > inliersResult.size()) {
+			inliersResult = inliers;
+		}
+	}
+	
+	
+	auto endTime = std::chrono::steady_clock::now();
+	auto elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+    std::cout << "Found " << inliersResult.size() << " inliers" << std::endl;
+	std::cout << "Line RANSAC elapsedTime: " << elapsedTime.count() << " micro-seconds." << std::endl;
+	
+	return inliersResult;
+}
+
+std::unordered_set<int> Ransac3DLineUsingPCL(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int maxIterations, float distanceTol)
+{
+	auto startTime = std::chrono::steady_clock::now();
+	srand(time(NULL)); // initialize random number generator
+
+	 // references http://pointclouds.org/documentation/tutorials/extract_indices.php#extract-indices
+    pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients ());
+    pcl::PointIndices::Ptr inliers (new pcl::PointIndices ());
+    // Create the segmentation object
+    pcl::SACSegmentation<pcl::PointXYZ> seg;
+    // Optional
+    seg.setOptimizeCoefficients(true);
+    // Mandatory
+    seg.setModelType(pcl::SACMODEL_PLANE);
+    seg.setMethodType(pcl::SAC_RANSAC);
+    seg.setMaxIterations(maxIterations);
+    seg.setDistanceThreshold(distanceTol);
+
+    seg.setInputCloud(cloud);
+    seg.segment(*inliers, *coefficients);
+    std::cout << "Found " << inliers->indices.size() << " inliers" << std::endl;
+	
+	auto endTime = std::chrono::steady_clock::now();
+	auto elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+	std::cout << "Line RANSAC elapsedTime: " << elapsedTime.count() << " micro-seconds." << std::endl;
+	
+	std::unordered_set<int> inliersResult(inliers->indices.begin(), inliers->indices.end());
+	return inliersResult;
 }
 
 int main ()
@@ -124,11 +262,17 @@ int main ()
 	pcl::visualization::PCLVisualizer::Ptr viewer = initScene();
 
 	// Create data
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData();
+	// pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData();
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData3D();
 	
 
+
 	// TODO: Change the max iteration and distance tolerance arguments for Ransac function
-	std::unordered_set<int> inliers = Ransac(cloud, 10, 0.5);
+	// std::unordered_set<int> inliers = Ransac2DLineUsingNormalDistance(cloud, 10, 0.5);
+	// std::unordered_set<int> inliers = Ransac2DLineUsingBounds(cloud, 10, 0.5);
+	std::unordered_set<int> inliers = Ransac3DLineUsingNormalDistance(cloud, 5, 0.5);
+	// std::unordered_set<int> inliers = Ransac3DLineUsingPCL(cloud, 5, 0.5);
+	
 
 	pcl::PointCloud<pcl::PointXYZ>::Ptr  cloudInliers(new pcl::PointCloud<pcl::PointXYZ>());
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloudOutliers(new pcl::PointCloud<pcl::PointXYZ>());
